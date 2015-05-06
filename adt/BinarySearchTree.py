73688680e88671a9f96cb82b56b01f2356fdf328
Herman Dempere <nerucius@gmail.com>
2015-05-06 10:14:40 +0200

    Added ADT folder, binarysearchtree

---
+++ adt/BinarySearchTree.py
@@ -0,0 +1,87 @@
+__author__ = 'Akira'
+
+from Node import *
+
+class BinarySearchTree(object):
+
+    def __init__(self):
+        self._root = None
+
+    def insert(self, item):
+        # TODO Rotate functions to keep the tree balanced
+        probe = self._root
+        while True:
+            print "inserting"
+            if probe is None:
+                self._root = Node(item)
+                return
+
+            # Insertion
+            if probe.prev() is None and item < probe.get_data():
+                probe.set_prev(Node(item))
+                probe.prev().set_parent(probe)
+                return
+            if probe.next() is None and item > probe.get_data():
+                probe.set_next(Node(item))
+                probe.next().set_parent(probe)
+                return
+
+            # Traversal
+            if item > probe.get_data():
+                probe = probe.next()
+            if item < probe.get_data():
+                probe = probe.prev()
+
+
+    def remove(self, node):
+            if node.next() is None and node.prev() is None:
+                node.parent.remove_child(node)
+
+            # TODO Finish remove from tree
+            pass
+
+
+    def contains(self, item, node=None):
+        """ Recursive function that returns True if the tree contains the given item. """
+        if self._root is None:
+            return False
+
+        if node is None:
+            node = self._root
+
+        if node.get_data() == item:
+            return True
+
+        elif item < node.get_data() and node.prev() is None:
+            return self.contains(node.prev, item)
+
+        elif item > node.get_data() and node.next() is None:
+            return self.contins(node.right, item)
+
+        return False
+
+    def __str__(self):
+        if self._root == None:
+            return "[Empty Tree]"
+        node = self._root
+
+        res = str(node) + self._get_str(node.prev()) + self._get_str(node.next())
+        return res
+
+
+    def _get_str(self, node):
+        # TODO Define a recursive method to get a string from a tree of nodes
+        pass
+
+    @staticmethod
+    def test():
+        tree = BinarySearchTree()
+        tree.insert("H")
+        tree.insert("A")
+        tree.insert("Z")
+        print tree.contains("Hello")
+
+        print tree
+
+if __name__ == "__main__":
+    BinarySearchTree.test()
